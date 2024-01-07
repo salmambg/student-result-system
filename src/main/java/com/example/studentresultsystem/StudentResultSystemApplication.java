cf2f986a85f4795f54bd6f66b95ac4e4e4726c2e
salma <gsalmahossain@gmail.com>
2024-01-07 21:59:37 +0100

    starting tets project

---
+++ src/main/java/com/example/studentresultsystem/StudentResultSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentresultsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentResultSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentresultsystem/StudentResultSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentresultsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentResultSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
